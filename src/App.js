c814d8c21bb39eff82b3c0899f9ff4eccc824ac1
Giuseppe Fierro <giuseppe.fierro@soundtrackyourbrand.com>
2022-03-16 10:18:50 +0100

    feat: setup css modules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Features from './components/features';
 
 function App() {
   return (
     <div className="App">
+      <Features />
     </div>
   );
 }
---
+++ src/components/features.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './features.module.css'
+
+export default function Features() {
+  return <section className={styles.container}>
+    <h1>Features overview</h1>
+
+  </section>
+}
